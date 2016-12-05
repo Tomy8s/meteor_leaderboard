2f94f3bd415b73708e5862f4dc9d2000ed833bef
Tomy8s <tomy8s@hotmail.com>
2016-12-05 17:47:25 +0000

    sort by score

--- leaderboard.js
+++ leaderboard.js
@@ -4,7 +4,7 @@ if(Meteor.isClient){
   console.log('Hello!');
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({}, {sort:{score: -1}});
     },
     'selectedClass': function(){
       var playerId = this._id;
