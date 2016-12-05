d6f41f45ec1c025d9555d768aca40372ce1d78f6
James <James.Steel@me.com>
2016-12-05 18:33:15 +0000

    players can log in

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,9 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find({}, {sort:{score: -1, name: 1}});
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({ createdBy: currentUserId},
+                              {sort:{score: -1, name: 1}});
     },
     'selectedClass': function(){
       var playerId = this._id;
@@ -20,7 +22,6 @@ if(Meteor.isClient){
   Template.leaderboard.events({
       'click .player' : function(){
         var playerId = this._id;
-          console.log(playerId);
           Session.set('selectedPlayer', playerId);
       },
       'click .increment': function(){
@@ -30,7 +31,24 @@ if(Meteor.isClient){
       'click .decrement': function(){
         var selectedPlayer = Session.get('selectedPlayer');
         PlayersList.update({ _id: selectedPlayer }, { $inc: { score: -5 }});
+      },
+      'click .remove': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.remove({ _id: selectedPlayer });
       }
+  });
+  Template.addPlayerForm.events({
+    'submit form': function(){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+      event.target.playerName.value = "";
+    }
   });
 }
 
