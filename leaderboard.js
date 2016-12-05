c31e271fc8818f8fc9532593ba2f7f49b16077cf
Tomy8s <tomy8s@hotmail.com>
2016-12-05 17:17:05 +0000

    added click event listener to .player class

--- leaderboard.js
+++ leaderboard.js
@@ -7,6 +7,14 @@ if(Meteor.isClient){
       return PlayersList.find();
     }
   })
+  Template.leaderboard.events({
+      'click .player' : function(){
+          console.log('Click' );
+          Session.set('selectedPlayer', 'Session Value test');
+          var selectedPlayer = Session.get('selectedPlayer');
+          console.log(selectedPlayer);
+      }
+  });
 }
 
 if(Meteor.isServer){
