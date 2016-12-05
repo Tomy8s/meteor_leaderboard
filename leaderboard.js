bdf770fd0c6c9228550eeecd2a054fd496bbc6b8
James <James.Steel@me.com>
2016-12-05 16:45:46 +0000

    names on page

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,14 @@
-console.log('Hi');
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  console.log('Hello!');
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    }
+  })
+}
+
+if(Meteor.isServer){
+  console.log('Server');
+}
